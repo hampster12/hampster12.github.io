9896ee4edde79a370b6360194f1c223581056eaa
Hannah <hamferry48@gmail.com>
2020-06-17 16:27:47 -0700

    voice background

--- pjs.js
+++ pjs.js
@@ -1,3 +1,14 @@
+//add images to arrays
+var voiceAlbum = ["images/voiceimages/01hannah.png","images/voiceimages/02hannah.png","images/voiceimages/03hannah.png","images/voiceimages/04hannah.png","images/voiceimages/05hannah.png","images/voiceimages/06hannah.png",];
+
 $(document).ready(function()
 {
+    //setUpVoice();
+    //function setUpVoice()
+    //{
+       // for(var i = 0; i < 6; i++)
+        //{
+          //  $("#voiceAdd").append('<div class="col-sm-12 image-cell"><div class="voiceImage"><img src = ' + voiceAlbum[i] + ' id="' + i + '"></div></div>');
+        //}
+   // }
 });
